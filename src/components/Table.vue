8132779549660c28471174ae3eebc5064e9eb6b6
basicactor <careergo357@gmail.com>
2022-06-14 12:58:03 +0900

    再利用可能なテーブル作成

---
+++ src/components/Table.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+      <template v-slot:item.actions="{ item }">
+        <slot name="actions" :id="item.id" />
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  props: {
+    headers: Array,
+    items: Array,
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
