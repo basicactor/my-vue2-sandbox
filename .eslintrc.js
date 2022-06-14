e92f8869b77f2795d16fe3ffe8959a1d7c5f2fb7
basicactor <careergo357@gmail.com>
2022-06-14 12:55:46 +0900

    lint+ prettier + vscode設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,12 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off", //追加
+    // semi: ["error", "never", { beforeStatementContinuationChars: "never" }],
+    // "semi-spacing": ["error", { after: true, before: false }],
+    // "semi-style": ["error", "first"],
+    // "no-extra-semi": "error",
+    // "no-unexpected-multiline": "error",
+    // "no-unreachable": "error",
+    "vue/valid-v-slot": ["error", { allowModifiers: true }],
   },
 }
