c5a86370de5f3019884f04f5a832a434163185ac
basicactor <careergo357@gmail.com>
2022-07-04 00:43:33 +0900

    メインラッパー実装

---
+++ src/layouts/MainWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="px-16" :style="{ width: '80%' }">
+    <slot />
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  setup() {},
+})
+</script>
