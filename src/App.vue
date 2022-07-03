eeecc5099b9bbabbdc33f6a1a5ad8af19d055788
basicactor <careergo357@gmail.com>
2022-07-04 08:37:12 +0900

    サイドナビ固定＋おしゃれに変更

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <Header />
     <v-main>
-      <div class="d-flex">
+      <div>
         <SideNavi />
         <MainWrapper>
           <BaseLoading />
--- src/layouts/SideNavi.vue
+++ src/layouts/SideNavi.vue
@@ -1,36 +1,48 @@
 <template>
-  <div class="ma-0 pa-0">
-    <v-card>
-      <v-navigation-drawer
-        permanent
-        height="auto"
-        :style="{ 'min-height': '100vh' }"
-      >
-        <v-list>
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img
-                src="https://randomuser.me/api/portraits/women/85.jpg"
-              ></v-img>
-            </v-list-item-avatar>
-            <v-list-item-title>x tarou</v-list-item-title>
-          </v-list-item>
-        </v-list>
-        <v-divider />
+  <!-- <div class="ma-0 pa-0" :style="{ position: 'fixed' }"> -->
+  <!-- <v-navigation-drawer
+    permanent
+    class="ma-0 pa-0"
+    expand-on-hover
+    height="auto"
+    :style="{
+      'min-height': '100vh',
+      position: 'fixed',
+      top: '65px',
+      'background-color': 'white',
+      'z-index': '10',
+      'backdrop-filter': 'blur(4px)',
+    }"
+  > -->
+  <v-navigation-drawer
+    class="ma-0 pa-0"
+    expand-on-hover
+    height="auto"
+    fixed
+    tag="custom-drawer"
+  >
+    <v-list class="mt-6">
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-title>x tarou</v-list-item-title>
+      </v-list-item>
+    </v-list>
+    <v-divider />
 
-        <v-list nav dense>
-          <template v-for="navi in navis">
-            <v-list-item :key="navi.link" :to="navi.link">
-              <v-list-item-icon>
-                <v-icon>{{ navi.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>{{ navi.title }}</v-list-item-title>
-            </v-list-item>
-          </template>
-        </v-list>
-      </v-navigation-drawer>
-    </v-card>
-  </div>
+    <v-list nav dense>
+      <template v-for="navi in navis">
+        <v-list-item :key="navi.link" :to="navi.link">
+          <v-list-item-icon>
+            <v-icon>{{ navi.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ navi.title }}</v-list-item-title>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -58,3 +70,20 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+custom-drawer {
+  min-height: 400px;
+  top: 10% !important;
+  background-color: white;
+  z-index: 10;
+  backdrop-filter: blur(4px);
+  border-radius: 0 100px 100px 0;
+  box-shadow: 3px 5px 5px #e2e2e2;
+
+  &.v-navigation-drawer--is-mouseover {
+    border-radius: 10px;
+    // min-height: 100vh;
+  }
+}
+</style>
