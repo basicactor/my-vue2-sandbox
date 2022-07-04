2bd34b07bfb963f9612bf69f41c6e7f6667ce9c5
basicactor <careergo357@gmail.com>
2022-07-04 22:56:26 +0900

    vuetifyテーマ指定

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,22 @@
 import Vue from "vue"
 import Vuetify from "vuetify/lib/framework"
+import colors from "vuetify/lib/util/colors" //定義済みマテリアルカラー
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        //https://vuetifyjs.com/ja/features/theme/#section-30ab30b930bf30de30a430ba
+        primary: colors.purple.darken1,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      dark: {
+        // primary: colors.blue.lighten3,
+      },
+    },
+  },
+})
