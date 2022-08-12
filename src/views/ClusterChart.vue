d9fa6a88c94a4b9efb9ab67c0f02aa41ed1b0693
basicactor <careergo357@gmail.com>
2022-08-13 00:37:33 +0900

    リファクタ

--- src/views/ClusterChart.vue
+++ src/views/ClusterChart.vue
@@ -2,65 +2,49 @@
   <div>
     <h1>hello</h1>
     <div ref="chartRef" class="white" style="height: 100vh" />
-    <v-pagination v-model="page" :length="10" @input="onChagePage" />
+    <v-pagination v-model="page" :length="maxPagingNum" @input="onChagePage" />
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from "@vue/composition-api"
-import { vis, Node, Network } from "vis-network"
+import { computed, defineComponent, onMounted, ref } from "@vue/composition-api"
+import { vis, Node, Network, DataSet } from "vis-network"
 
-const originData = [...Array(100)].reduce((pv, cv, idx) => {
+const originData = [...Array(30)].reduce((pv, cv, idx) => {
   let cid = pv.slice(-1)[0]?.cid ?? 0 //初期値は空配列で要素取得するとundefinedになるため0を設定する
   cid = idx % 5 ? cid : cid + 1 //n個以上になったらcidを１上げる
   return [...pv, { query: `test${idx + 1}`, cid }]
 }, [])
-console.log("originData", originData)
 
 export default defineComponent({
   setup() {
     const chartRef = ref()
     const page = ref(1)
-
-    // const nodes = []
-    // // const edges = []
-
-    // originData.forEach((i, index) => {
-    //   //1. 初めてのcidの場合は、新たな配列を作る＝＞
-    //   //[{nodes: [{id:1, label,group:1} x group10個分], edges: [from:"cid", to:自分]}]
-    //   //2. cidが既に存在する場合は、既存の配列に追加する
-    //   //3. cidごとに作られた配列の数が10個になったら、次の配列に移行する
-    //   const node = {
-    //     id: index + 1,
-    //     label: i.query,
-    //     group: i.cid,
-    //   }
-
-    //   nodes.push(node)
-    // })
+    const maxPagingNum = computed(() => chunkedLists.length) //最大ページ数
 
     //全てのnodesとedgesを持つオブジェクト配列
-    const baseDataSet = originData.reduce(
-      (pv, cv, idx) => {
-        const obj = pv[cv.cid] || (pv[cv.cid] = { nodes: [], edges: [] })
-        // lastStep: cidが10個溜まったら、nodes配列とedges配列にそれぞれpushして、次の配列に移る。
-        const { nodes, edges } = obj
-        nodes.push({
-          id: idx + 1,
-          label: cv.query,
-          group: cv.cid,
-        })
+    const baseDataSet = originData
+      .reduce(
+        (pv, cv, idx) => {
+          if (idx === 0) return pv
+          const obj = pv[cv.cid] || (pv[cv.cid] = { nodes: [], edges: [] })
+          obj.nodes.push({
+            id: idx + 1,
+            label: cv.query,
+            group: cv.cid, //cidをgroup分け用に使用
+          })
 
-        const groupNodeId = nodes[0].id //グループの1番目要素のID
-        //fromとtoのIDが同じ場合は、edgeオブジェクトを生成しない。
-        edges.push({
-          from: groupNodeId === idx + 1 ? "root" : groupNodeId,
-          to: idx + 1,
-        })
-        return pv
-      },
-      [{ nodes: [], edges: [] }] //初期オブジェクト配列
-    )
+          const groupNodeId = obj.nodes[0].id //グループの1番目要素のID
+          obj.edges.push({
+            //fromとtoのIDが同じ場合は、fromにrootを指定する
+            from: groupNodeId === idx + 1 ? "root" : groupNodeId,
+            to: idx + 1,
+          })
+          return pv
+        },
+        [] //初期オブジェクト配列
+      )
+      .filter((i) => i) //filterすることで、配列一番目要素のemptyを排除する。
     console.log("baseDataSet", baseDataSet)
 
     //配列をN個ごとに分割する
@@ -70,38 +54,35 @@ export default defineComponent({
         []
       )
     }
-    const chunkedLists = chunkArray(baseDataSet, 10).map((i) =>
-      i.reduce(
-        (pv, cv, idx) => {
+
+    //各配列のnodesとedgesを各々統合する
+    const combineDataSetByGroup = (array) => {
+      return array.reduce(
+        (pv, cv) => {
           pv.nodes.push(...cv.nodes)
           pv.edges.push(...cv.edges)
           return pv
         },
         { nodes: [], edges: [] }
       )
+    }
+
+    //cid10個ずつ配列に分割し、配列内のnodesとedgesをまとめる
+    const chunkedLists = chunkArray(baseDataSet, 10).map((i) =>
+      combineDataSetByGroup(i)
     )
     console.log("chunkedLists", chunkedLists)
 
-    // const groupBy = (array, pName) => {
-    //   return array.reduce((pv, cv, index, src) => {
-    //     const key = src[pName]
-    //     ;(pv[key] || (pv[key] = [])).push(cv)
-    //     return pv
-    //   }, {})
-    // }
-
-    // const groupedNode = groupBy(nodes, "group")
-    // console.log("groupedNode", groupedNode)
-
+    //初期画面表示時＋ページ変更時に発火。
+    //ページindexに合ったデータを取り出し、new netWorkする。
     const onChagePage = (pageNumber = 1) => {
       const rootNode = [{ id: "root", label: "root" }]
 
       const nodes = rootNode.concat(chunkedLists[pageNumber - 1].nodes)
       const edges = chunkedLists[pageNumber - 1].edges
 
-      console.log("nodes", nodes)
-
-      console.log("edges", edges)
+      // console.log("nodes", nodes)
+      // console.log("edges", edges)
 
       new Network(chartRef.value, { nodes, edges }, {})
     }
@@ -110,7 +91,7 @@ export default defineComponent({
       onChagePage()
     })
 
-    return { chartRef, page, onChagePage }
+    return { chartRef, page, maxPagingNum, onChagePage }
   },
 })
 </script>
