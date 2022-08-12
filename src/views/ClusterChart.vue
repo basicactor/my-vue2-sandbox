6808343179aef2c3185758c23bf4e8d88c7e8d9f
basicactor <careergo357@gmail.com>
2022-08-13 01:17:20 +0900

    クラスターマップ：TS化完了

--- src/views/ClusterChart.vue
+++ src/views/ClusterChart.vue
@@ -2,19 +2,31 @@
   <div>
     <h1>hello</h1>
     <div ref="chartRef" class="white" style="height: 100vh" />
-    <v-pagination v-model="page" :length="maxPagingNum" @input="onChagePage" />
+    <v-pagination
+      v-model="page"
+      :length="maxPagingNum"
+      @input="drawClusterMap"
+    />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { computed, defineComponent, onMounted, ref } from "@vue/composition-api"
-import { vis, Node, Network, DataSet } from "vis-network"
+import { Node, Network, Edge } from "vis-network"
 
-const originData = [...Array(30)].reduce((pv, cv, idx) => {
+const originData: Array<{ query: string; cid: number }> = [
+  ...Array(200),
+].reduce((pv, cv, idx) => {
   let cid = pv.slice(-1)[0]?.cid ?? 0 //初期値は空配列で要素取得するとundefinedになるため0を設定する
-  cid = idx % 5 ? cid : cid + 1 //n個以上になったらcidを１上げる
+  cid = idx % 10 ? cid : cid + 1 //n個以上になったらcidを１上げる
   return [...pv, { query: `test${idx + 1}`, cid }]
 }, [])
+
+//本家のData型を使うと、nodesとedgesを使うときに毎回分岐処理が走るため、型を独自定義する
+type DataNodeEdge = {
+  nodes: Array<Node>
+  edges: Array<Edge>
+}
 
 export default defineComponent({
   setup() {
@@ -23,32 +35,37 @@ export default defineComponent({
     const maxPagingNum = computed(() => chunkedLists.length) //最大ページ数
 
     //全てのnodesとedgesを持つオブジェクト配列
-    const baseDataSet = originData
+    const baseDataNodeEdge: Array<DataNodeEdge> = originData
       .reduce(
-        (pv, cv, idx) => {
+        (pv: Array<DataNodeEdge>, cv, idx) => {
           if (idx === 0) return pv
-          const obj = pv[cv.cid] || (pv[cv.cid] = { nodes: [], edges: [] })
+          const obj =
+            pv[cv.cid] ||
+            (pv[cv.cid] = {
+              nodes: [] as Array<Node>,
+              edges: [] as Array<Edge>,
+            })
           obj.nodes.push({
             id: idx + 1,
             label: cv.query,
-            group: cv.cid, //cidをgroup分け用に使用
-          })
+            group: `${cv.cid}`, //cidをgroup分け用に使用
+          } as Node)
 
           const groupNodeId = obj.nodes[0].id //グループの1番目要素のID
           obj.edges.push({
             //fromとtoのIDが同じ場合は、fromにrootを指定する
             from: groupNodeId === idx + 1 ? "root" : groupNodeId,
             to: idx + 1,
-          })
+          } as Edge)
           return pv
         },
         [] //初期オブジェクト配列
       )
-      .filter((i) => i) //filterすることで、配列一番目要素のemptyを排除する。
-    console.log("baseDataSet", baseDataSet)
+      .filter((i: DataNodeEdge) => i) //filterすることで、配列一番目要素のemptyを排除する。
+    console.log("baseDataNodeEdge", baseDataNodeEdge)
 
     //配列をN個ごとに分割する
-    const chunkArray = (array, num) => {
+    const chunkArray = (array: Array<any>, num: number) => {
       return array.reduce(
         (pv, _, idx) => (idx % num ? pv : [...pv, array.slice(idx, idx + num)]),
         []
@@ -56,7 +73,9 @@ export default defineComponent({
     }
 
     //各配列のnodesとedgesを各々統合する
-    const combineDataSetByGroup = (array) => {
+    const combineDataNodeEdgeByGroup = (
+      array: Array<DataNodeEdge>
+    ): DataNodeEdge => {
       return array.reduce(
         (pv, cv) => {
           pv.nodes.push(...cv.nodes)
@@ -68,18 +87,20 @@ export default defineComponent({
     }
 
     //cid10個ずつ配列に分割し、配列内のnodesとedgesをまとめる
-    const chunkedLists = chunkArray(baseDataSet, 10).map((i) =>
-      combineDataSetByGroup(i)
+    const chunkedLists = chunkArray(baseDataNodeEdge, 10).map(
+      (i: Array<DataNodeEdge>) => combineDataNodeEdgeByGroup(i)
     )
     console.log("chunkedLists", chunkedLists)
 
     //初期画面表示時＋ページ変更時に発火。
     //ページindexに合ったデータを取り出し、new netWorkする。
-    const onChagePage = (pageNumber = 1) => {
-      const rootNode = [{ id: "root", label: "root" }]
+    const drawClusterMap = (pageNumber = 1): void => {
+      const rootNode: Array<Node> = [{ id: "root", label: "root" }]
 
-      const nodes = rootNode.concat(chunkedLists[pageNumber - 1].nodes)
-      const edges = chunkedLists[pageNumber - 1].edges
+      const nodes: Array<Node> = rootNode.concat(
+        chunkedLists[pageNumber - 1].nodes
+      )
+      const edges: Array<Edge> = chunkedLists[pageNumber - 1].edges
 
       // console.log("nodes", nodes)
       // console.log("edges", edges)
@@ -88,10 +109,10 @@ export default defineComponent({
     }
 
     onMounted(() => {
-      onChagePage()
+      drawClusterMap()
     })
 
-    return { chartRef, page, maxPagingNum, onChagePage }
+    return { chartRef, page, maxPagingNum, drawClusterMap }
   },
 })
 </script>
