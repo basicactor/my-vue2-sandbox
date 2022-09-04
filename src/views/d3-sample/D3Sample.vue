4b035d1e1d59c4cf0c0a09fd86612f18ac88a46b
basicactor <careergo357@gmail.com>
2022-09-04 23:57:28 +0900

    d3-drag: arrow関数を使うバージョン

--- src/views/d3-sample/D3Sample.vue
+++ src/views/d3-sample/D3Sample.vue
@@ -8,16 +8,12 @@
         :r="cr"
         stroke="black"
         fill="yellow"
-        @mousedown="dragStart($event)"
-        @mousemove="dragMove($event)"
-        @mouseup="dragEnd($event)"
-        @mouseleave="dragEnd($event)"
       />
     </svg>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent, onMounted, ref } from "@vue/composition-api"
 import * as d3 from "d3"
 
@@ -27,32 +23,40 @@ export default defineComponent({
     const cy = ref(50) //y座標
     const cr = 40 //半径
 
-    const isDragging = ref(false)
+    // const isDragging = ref(false)
 
-    const dragStart = () => {
-      isDragging.value = true
-    }
+    // const dragStart = () => {
+    //   isDragging.value = true
+    // }
 
-    const dragMove = (event: MouseEvent) => {
-      if (isDragging.value) {
-        cx.value = event.offsetX
-        cy.value = event.offsetY
-      } else return
-    }
+    // const dragMove = (event: MouseEvent) => {
+    //   if (isDragging.value) {
+    //     cx.value = event.offsetX
+    //     cy.value = event.offsetY
+    //   } else return
+    // }
 
-    const dragEnd = () => {
-      isDragging.value = false
-    }
+    // const dragEnd = () => {
+    //   isDragging.value = false
+    // }
 
     onMounted(() => {
-      // d3.select("#my-circle").call(
-      //   d3.drag().on("drag", function (e) {
-      //     d3.select(this).attr("cx", e.x).attr("cy", e.y)
-      //   })
-      // )
+      d3.select("#my-circle").call(
+        d3.drag().on("drag", (e) => {
+          //arrow関数ではthisの代わりにe.sourceEvent.targetを使う
+          d3.select(e.sourceEvent.target).attr("cx", e.x).attr("cy", e.y)
+        })
+      )
     })
 
-    return { cx, cy, cr, dragStart, dragMove, dragEnd }
+    return {
+      cx,
+      cy,
+      cr,
+      // dragStart,
+      // dragMove,
+      //  dragEnd
+    }
   },
 })
 </script>
