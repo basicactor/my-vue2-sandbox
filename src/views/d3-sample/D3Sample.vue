98db267dba846d7b2b4baa6c9e7fb2b5e4e41168
basicactor <careergo357@gmail.com>
2022-09-04 23:43:55 +0900

    mouseイベントでドラッグ実装

--- src/views/d3-sample/D3Sample.vue
+++ src/views/d3-sample/D3Sample.vue
@@ -8,30 +8,51 @@
         :r="cr"
         stroke="black"
         fill="yellow"
+        @mousedown="dragStart($event)"
+        @mousemove="dragMove($event)"
+        @mouseup="dragEnd($event)"
+        @mouseleave="dragEnd($event)"
       />
     </svg>
   </div>
 </template>
 
-<script>
-import { defineComponent, onMounted } from "@vue/composition-api"
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "@vue/composition-api"
 import * as d3 from "d3"
 
 export default defineComponent({
   setup() {
-    const cx = 50 //x座標
-    const cy = 50 //y座標
+    const cx = ref(50) //x座標
+    const cy = ref(50) //y座標
     const cr = 40 //半径
 
+    const isDragging = ref(false)
+
+    const dragStart = () => {
+      isDragging.value = true
+    }
+
+    const dragMove = (event: MouseEvent) => {
+      if (isDragging.value) {
+        cx.value = event.offsetX
+        cy.value = event.offsetY
+      } else return
+    }
+
+    const dragEnd = () => {
+      isDragging.value = false
+    }
+
     onMounted(() => {
-      d3.select("#my-circle").call(
-        d3.drag().on("drag", function (e) {
-          d3.select(this).attr("cx", e.x).attr("cy", e.y)
-        })
-      )
+      // d3.select("#my-circle").call(
+      //   d3.drag().on("drag", function (e) {
+      //     d3.select(this).attr("cx", e.x).attr("cy", e.y)
+      //   })
+      // )
     })
 
-    return { cx, cy, cr }
+    return { cx, cy, cr, dragStart, dragMove, dragEnd }
   },
 })
 </script>
