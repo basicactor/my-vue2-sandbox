8deef483244c0adec853af199219f81d71d687ba
basicactor <careergo357@gmail.com>
2022-09-04 22:53:37 +0900

    d3でドラッグ実装

--- src/views/d3-sample/D3Sample.vue
+++ src/views/d3-sample/D3Sample.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <svg>
-      <circle :cx="cx" :cy="cy" :r="cr" stroke="black" fill="yellow" />
+    <svg height="500" width="500" style="background-color: white">
+      <circle
+        id="my-circle"
+        :cx="cx"
+        :cy="cy"
+        :r="cr"
+        stroke="black"
+        fill="yellow"
+      />
     </svg>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "@vue/composition-api"
+<script>
+import { defineComponent, onMounted } from "@vue/composition-api"
 import * as d3 from "d3"
 
 export default defineComponent({
@@ -15,6 +22,14 @@ export default defineComponent({
     const cx = 50 //x座標
     const cy = 50 //y座標
     const cr = 40 //半径
+
+    onMounted(() => {
+      d3.select("#my-circle").call(
+        d3.drag().on("drag", function (e) {
+          d3.select(this).attr("cx", e.x).attr("cy", e.y)
+        })
+      )
+    })
 
     return { cx, cy, cr }
   },
