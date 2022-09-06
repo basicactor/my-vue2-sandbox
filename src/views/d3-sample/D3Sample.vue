5257b2f1f19336a48094869f182b2857a9ac03b0
basicactor <careergo357@gmail.com>
2022-09-06 22:59:01 +0900

    d3ズーム：スクロールでズーム実装＋なぜかDragも一緒に実装できた。

--- src/views/d3-sample/D3Sample.vue
+++ src/views/d3-sample/D3Sample.vue
@@ -3,7 +3,8 @@
     <D3Drag />
     <TooltipCloseOutside />
     <SaveImage />
-    <D3Zoom />
+    <ZoomD3 />
+    <ZoomVanila v-if="false" />
   </div>
 </template>
 
@@ -12,14 +13,16 @@ import { defineComponent } from "@vue/composition-api"
 import D3Drag from "./components/D3Drag.vue"
 import SaveImage from "./components/SaveImage.vue"
 import TooltipCloseOutside from "./components/TooltipCloseOutside.vue"
-import D3Zoom from "./components/D3Zoom.vue"
+import ZoomD3 from "./components/ZoomD3.vue"
+import ZoomVanila from "./components/ZoomVanila.vue"
 
 export default defineComponent({
   components: {
     D3Drag,
     TooltipCloseOutside,
     SaveImage,
-    D3Zoom,
+    ZoomD3,
+    ZoomVanila,
   },
   setup() {
     return {}
---
+++ src/views/d3-sample/components/ZoomD3.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <svg
+      id="svg-image-zoom"
+      :width="width"
+      :height="height"
+      style="background-color: white"
+    >
+      <!-- rectがないとダウンロードしたsvgのサイズが画面最大サイズになる -->
+      <g id="svg-image-group">
+        <!-- <rect width="100%" height="100%" fill="red" /> -->
+        <circle :r="50" stroke="black" fill="yellow" cursor="pointer" />
+      </g>
+    </svg>
+    <v-btn @click="resetZoom">reset</v-btn>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from "@vue/composition-api"
+import * as d3 from "d3"
+
+export default defineComponent({
+  setup() {
+    const width = 500
+    const height = 500
+
+    //これでなぜがDragも出来る！！！
+    const initView = () => {
+      // Init SVG container
+      const svg = d3.select("#svg-image-zoom")
+      const container = d3.select("#svg-image-group")
+
+      // Init D3 zoom
+      const zoom = d3
+        .zoom()
+        .scaleExtent([0.1, 10]) //[最小サイズ、最大サイズ]
+        .on("zoom", (event) => container.attr("transform", event.transform))
+
+      svg.call(zoom)
+
+      // Center container within SVG
+      const centered = d3.zoomIdentity.translate(width / 2, height / 2)
+      svg.call(zoom.transform, centered)
+    }
+
+    //zoom倍率をリセット
+    const resetZoom = () => {
+      const imageGroup = d3.select("#svg-image-group")
+      imageGroup.attr("transform", `translate(${width / 2},${height / 2})`)
+    }
+
+    onMounted(() => {
+      initView()
+    })
+    return { width, height, resetZoom }
+  },
+})
+</script>
